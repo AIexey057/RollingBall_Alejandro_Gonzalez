be0f01830c388517dce40bbb183a7254cd888c65
AIexey057 <alexgonzalezgarcia2005@gmail.com>
2024-10-02 20:41:05 +0200

    MovimientoBoola

---
+++ Assets/Srcipts/Boloncho.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boloncho : MonoBehaviour
+{
+    Vector3 direccion = new Vector3 (1, 0, 0);
+    [SerializeField]float velocidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * velocidad * Time.deltaTime, Space.World);
+    }
+}
