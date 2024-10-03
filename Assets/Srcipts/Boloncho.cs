9b2f4c2ea50caa32ce544b5b0bc18d08b0d31b2f
AIexey057 <alexgonzalezgarcia2005@gmail.com>
2024-10-03 21:25:45 +0200

    fkjshdoaw

--- Assets/Srcipts/Boloncho.cs
+++ Assets/Srcipts/Boloncho.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class Boloncho : MonoBehaviour
 {
-    Vector3 direccion = new Vector3 (1, 0, 0);
+    Rigidbody rb;
+    Vector3 direccion = new Vector3 (0, 1, 0);
     [SerializeField]float velocidad;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-
-        transform.Translate(new Vector3(h, 0, v) * velocidad * Time.deltaTime, Space.World);
+        float v = Input.GetAxisRaw("vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           rb.AddForce(direccion * velocidad, ForceMode.Impulse);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(direccion * velocidad, ForceMode.Force);
+        }
+        
+        
     }
 }
