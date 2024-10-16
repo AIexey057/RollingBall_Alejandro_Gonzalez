9adb1129548ae48d49e8044f175dfebc44694a4b
AIexey057 <alexgonzalezgarcia2005@gmail.com>
2024-10-16 20:39:58 +0200

    16/10

--- Assets/Srcipts/MuroCubos.cs
+++ Assets/Srcipts/MuroCubos.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class MuroCubos : MonoBehaviour
 {
+   [SerializeField] private Rigidbody[] rbs;
+
+    private bool iniciarCuenta = false;
+    private float timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,15 +16,27 @@ public class MuroCubos : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+   private void Update()
     {
-        
+      if (iniciarCuenta == true)
+        {
+            timer += 1 * Time.unscaledDeltaTime;
+            if(timer >= 2)
+            {
+                Time.timeScale = 1f;
+               for(int i = 0; i < rbs.Length; i++)
+                {
+                    rbs[i].useGravity = true;
+                }
+            }
+        }  
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Time.timeScale = 0.1f;
+            Time.timeScale = 0.2f;
+            iniciarCuenta = true;  
         }
     }
 }
