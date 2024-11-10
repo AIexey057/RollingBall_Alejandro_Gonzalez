ec204fb4120ada0795dcbc87dd6c588395367f3f
AIexey057 <alexgonzalezgarcia2005@gmail.com>
2024-11-10 15:12:40 +0100

    Update Boloncho.cs

--- Assets/Srcipts/Boloncho.cs
+++ Assets/Srcipts/Boloncho.cs
@@ -68,8 +68,9 @@ public class Boloncho : MonoBehaviour
             vida -= 10;
             if (vida <= 0)
             {
-                StartCoroutine(HandleDeath());
                 Destroy(gameObject);
+                Debug.Log("Has Muerto ");
+                SceneManager.LoadScene(2);
             }
         }
 
@@ -82,18 +83,14 @@ public class Boloncho : MonoBehaviour
             vida -= 10;
             if (vida <= 0)
             {
-                StartCoroutine(HandleDeath());
                 Destroy(gameObject);
+                Debug.Log("Has Muerto ");
+                SceneManager.LoadScene(2);
             }
         }
     }
 
-    private IEnumerator HandleDeath()
-    {
-        Debug.Log("Has Muerto");
-        yield return new WaitForSeconds(1.5f);  // Lo busque de un tutorial
-        SceneManager.LoadScene(2);
-    }
+    
 
 
 }
